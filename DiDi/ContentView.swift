60b432f87dff15a4c11d549322313f6c9f282ae1
cooper <15204036698@163.com>
2019-10-21 14:44:44 +0800

    Initial Commit

---
+++ DiDi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiDi
+//
+//  Created by apple on 2019/10/21.
+//  Copyright © 2019 HeZhiBo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
